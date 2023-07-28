61f1fa774e81171a774deb3978dbef4c23a2be99
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-07-28 12:55:32 +0530

    improved add linkedin page

--- app/app/add-linkedin/page.tsx
+++ app/app/add-linkedin/page.tsx
@@ -13,14 +13,14 @@ export default function Page() {
   const { data: session, status } = useSession()
   const router = useRouter()
 
-  useEffect(() => {
-    if (status !== "authenticated") {
-      router.push("/app/login")
-    }
-    if (session?.user.added_linkedin) {
-      router.push("/app/settings")
-    }
-  })
+  // useEffect(() => {
+  //   if (status !== "authenticated") {
+  //     router.push("/app/login")
+  //   }
+  //   if (session?.user.added_linkedin) {
+  //     router.push("/app/settings")
+  //   }
+  // })
 
   function handleLinkedinChange(event: React.ChangeEvent<HTMLInputElement>) {
     setLinkedinURL(event.target.value)
@@ -41,13 +41,13 @@ export default function Page() {
 
   return (
     <main className='linkedin-url-page'>
-      <div className='max-w-website mx-auto flex flex-col items-center py-24'>
-        <h1 className='text-3xl font-medium capitalize mb-10'>
+      <div className='max-w-website mx-auto flex flex-col items-center py-24 px-4'>
+        <h1 className='text-3xl font-medium capitalize mb-10 text-center lg:text-2xl sm:text-xl'>
           Add your LinkedIn profile URL
         </h1>
         <input
           type='text'
-          className='p-2 border border-gray-300 rounded w-96 mb-8'
+          className='p-2 border border-gray-300 rounded w-96 mb-8 lg:w-80 sm:w-72'
           placeholder='https://www.linkedin.com/in/...'
           value={linkedinURL}
           disabled={loading}
