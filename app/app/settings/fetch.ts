6301e9382cc4afd4f741fec452b20b0b4e2601e3
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-08-12 14:36:29 +0530

    url fix

--- app/app/settings/fetch.ts
+++ app/app/settings/fetch.ts
@@ -1,12 +1,9 @@
 import HttpStatus from "@/constants/http_status"
 
 async function getUserInfo() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/site/settings`,
-    {
-      method: "GET",
-    }
-  )
+  const res = await fetch(`/api/site/settings`, {
+    method: "GET",
+  })
   if (res.ok && res.status === HttpStatus.SUCCESS) {
     const data = await res.json()
     return data
