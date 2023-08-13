e1ad9146d4c7624ef2955cfa9e137ab446e3fb1c
Aditya Narayan Rai <raiadi2008@gmail.com>
2023-08-13 18:47:38 +0530

    profile username data

--- components/settings/profile/profile.tsx
+++ components/settings/profile/profile.tsx
@@ -1,17 +1,24 @@
 "use client"
 
 import HttpStatus from "@/constants/http_status"
+import useDebouncer from "@/hooks/useDebouncer"
 import { parseSiteDataFromJSON } from "@/utils/functions"
 import { ISite, ISiteUpdates } from "@/utils/interfaces"
 import Link from "next/link"
-import { Dispatch, SetStateAction, use, useEffect, useState } from "react"
-import { json } from "stream/consumers"
+import { useEffect, useState } from "react"
 
 interface ProfileInputError {
   first_name: boolean
   last_name: boolean
   username: boolean
   occupation: boolean
+}
+
+enum UsernameAvailablityCheck {
+  AVAILABLE = "available",
+  UNAVAILABLE = "unavailable",
+  LOADING = "loading",
+  IDLE = "idle",
 }
 
 export default function ProfileSettings({
@@ -35,11 +42,43 @@ export default function ProfileSettings({
       occupation: false,
     }
   )
+  const username = useDebouncer(profileInfo.username, 500)
+  const [usernameAvailablity, setUsernameAvailablity] =
+    useState<UsernameAvailablityCheck>(UsernameAvailablityCheck.IDLE)
 
   const portfolioURL = new URL(
     siteInfo.username!,
     process.env.NEXT_PUBLIC_BASE_URL
   )
+
+  useEffect(() => {
+    async function checkUsernameAvailablity() {
+      if (username.length < 4 || username === siteInfo.username) {
+        setUsernameAvailablity(UsernameAvailablityCheck.IDLE)
+        return
+      }
+      setUsernameAvailablity(UsernameAvailablityCheck.LOADING)
+      const res = await fetch(`/api/site/username?username=${username}`)
+      if (res.ok && res.status === HttpStatus.SUCCESS) {
+        const data = await res.json()
+        console.log("data", data)
+        if (data["available"]) {
+          setUsernameAvailablity(UsernameAvailablityCheck.AVAILABLE)
+        } else {
+          setUsernameAvailablity(UsernameAvailablityCheck.UNAVAILABLE)
+        }
+      }
+      console.log("username availabality", usernameAvailablity)
+    }
+    checkUsernameAvailablity()
+      .then()
+      .catch((e) => setUsernameAvailablity(UsernameAvailablityCheck.IDLE))
+
+    return () => {
+      checkUsernameAvailablity
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [username])
 
   useEffect(() => {
     if (
@@ -113,33 +152,40 @@ export default function ProfileSettings({
                 <span className='block'>on the portfolio site as required</span>
               </p>
             </div>
-            <input
-              className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
-                profileInputError.first_name
-                  ? "border-b border-neutral-red"
-                  : "focus:border-gray-300"
-              } `}
-              value={profileInfo.first_name}
-              onChange={(e) => {
-                const value = e.target.value
-                if (value.length === 0 && !profileInputError.first_name) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    first_name: true,
+            <div>
+              <input
+                className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
+                  profileInputError.first_name
+                    ? "border-b border-neutral-red"
+                    : "focus:border-gray-300"
+                } `}
+                value={profileInfo.first_name}
+                onChange={(e) => {
+                  const value = e.target.value
+                  if (value.length === 0 && !profileInputError.first_name) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      first_name: true,
+                    })
+                  }
+                  if (value.length > 0 && profileInputError.first_name) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      first_name: false,
+                    })
+                  }
+                  setProfileInfo({
+                    ...profileInfo,
+                    first_name: value,
                   })
-                }
-                if (value.length > 0 && profileInputError.first_name) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    first_name: false,
-                  })
-                }
-                setProfileInfo({
-                  ...profileInfo,
-                  first_name: value,
-                })
-              }}
-            />
+                }}
+              />
+              {profileInputError.first_name && (
+                <p className='text-xs font-extralight text-dark-red'>
+                  first name can&apos;t be empty
+                </p>
+              )}
+            </div>
           </div>
           <div className='flex items-center justify-between mb-8'>
             <div>
@@ -151,33 +197,40 @@ export default function ProfileSettings({
                 <span className='block'>on the portfolio site as required</span>
               </p>
             </div>
-            <input
-              className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
-                profileInputError.last_name
-                  ? "border-b border-neutral-red"
-                  : "focus:border-gray-300"
-              } `}
-              value={profileInfo.last_name}
-              onChange={(e) => {
-                const value = e.target.value
-                if (value.length === 0 && !profileInputError.last_name) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    last_name: true,
+            <div>
+              <input
+                className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
+                  profileInputError.last_name
+                    ? "border-b border-neutral-red"
+                    : "focus:border-gray-300"
+                } `}
+                value={profileInfo.last_name}
+                onChange={(e) => {
+                  const value = e.target.value
+                  if (value.length === 0 && !profileInputError.last_name) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      last_name: true,
+                    })
+                  }
+                  if (value.length > 0 && profileInputError.last_name) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      last_name: false,
+                    })
+                  }
+                  setProfileInfo({
+                    ...profileInfo,
+                    last_name: value,
                   })
-                }
-                if (value.length > 0 && profileInputError.last_name) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    last_name: false,
-                  })
-                }
-                setProfileInfo({
-                  ...profileInfo,
-                  last_name: value,
-                })
-              }}
-            />
+                }}
+              />
+              {profileInputError.last_name && (
+                <p className='text-xs font-extralight text-dark-red'>
+                  last name can&apos;t be empty
+                </p>
+              )}
+            </div>
           </div>
           <div className='flex items-center justify-between mb-8'>
             <div>
@@ -191,33 +244,40 @@ export default function ProfileSettings({
                 </span>
               </p>
             </div>
-            <input
-              className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
-                profileInputError.occupation
-                  ? "border-b border-neutral-red"
-                  : "focus:border-gray-300"
-              } `}
-              value={profileInfo.occupation}
-              onChange={(e) => {
-                const value = e.target.value
-                if (value.length === 0 && !profileInputError.occupation) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    occupation: true,
+            <div>
+              <input
+                className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
+                  profileInputError.occupation
+                    ? "border-b border-neutral-red"
+                    : "focus:border-gray-300"
+                } `}
+                value={profileInfo.occupation}
+                onChange={(e) => {
+                  const value = e.target.value
+                  if (value.length === 0 && !profileInputError.occupation) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      occupation: true,
+                    })
+                  }
+                  if (value.length > 0 && profileInputError.occupation) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      occupation: false,
+                    })
+                  }
+                  setProfileInfo({
+                    ...profileInfo,
+                    occupation: value,
                   })
-                }
-                if (value.length > 0 && profileInputError.occupation) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    occupation: false,
-                  })
-                }
-                setProfileInfo({
-                  ...profileInfo,
-                  occupation: value,
-                })
-              }}
-            />
+                }}
+              />
+              {profileInputError.occupation && (
+                <p className='text-xs font-extralight text-dark-red'>
+                  occupation can&apos;t be empty
+                </p>
+              )}
+            </div>
           </div>
           <div className='flex justify-between items-center mb-2'>
             <div>
@@ -231,33 +291,50 @@ export default function ProfileSettings({
                 </span>
               </p>
             </div>
-            <input
-              className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
-                profileInputError.username
-                  ? "border-b border-neutral-red"
-                  : "focus:border-gray-300"
-              } `}
-              value={profileInfo.username}
-              onChange={(e) => {
-                const value = e.target.value
-                if (value.length === 0 && !profileInputError.username) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    username: true,
+            <div>
+              <input
+                className={`text-gray-500 text-right outline-none focus:border-b focus:text-neutral-dark  py-1 px-2 focus:bg-gray-100 rounded-t w-72 focus:text-left ${
+                  profileInputError.username
+                    ? "border-b border-neutral-red"
+                    : "focus:border-gray-300"
+                } `}
+                value={profileInfo.username}
+                onChange={(e) => {
+                  const value = e.target.value
+                  if (value.length < 4) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      username: true,
+                    })
+                  }
+                  if (value.length >= 4 && profileInputError.username) {
+                    setProfileInputError({
+                      ...profileInputError,
+                      username: false,
+                    })
+                  }
+                  setProfileInfo({
+                    ...profileInfo,
+                    username: value,
                   })
-                }
-                if (value.length > 0 && profileInputError.username) {
-                  setProfileInputError({
-                    ...profileInputError,
-                    username: false,
-                  })
-                }
-                setProfileInfo({
-                  ...profileInfo,
-                  username: value,
-                })
-              }}
-            />
+                }}
+              />
+              {profileInputError.username && (
+                <p className='text-xs font-extralight text-dark-red'>
+                  username should be atleast 4 characters long
+                </p>
+              )}
+              {usernameAvailablity === UsernameAvailablityCheck.AVAILABLE && (
+                <p className='text-xs font-extralight text-green-800'>
+                  username is available
+                </p>
+              )}
+              {usernameAvailablity === UsernameAvailablityCheck.UNAVAILABLE && (
+                <p className='text-xs font-extralight  text-dark-red'>
+                  username is not available
+                </p>
+              )}
+            </div>
           </div>
           <div className='flex gap-x-12'>
             <Link href={portfolioURL.toString()} target='_blank'>
